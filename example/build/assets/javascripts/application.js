c2e3abb08abc21050497d9125acc54ce34189bef
Yavor Ivanov <yavor.ivanov@me.com>
2017-07-31 02:40:38 +0300

    WIP example app - tool for generating paginator options and preview

--- example/build/assets/javascripts/application.js
+++ example/build/assets/javascripts/application.js
@@ -1,69 +1,40 @@
 'use strict';
 
-var options = {
-  callback: yourCallback,
-  totalItems: 1000,
-  itemsPerPage: 10,
-  currentPage: 50,
-  options: {
-    outerPagesCount: 2,
-    innerPagesCount: 2
-  },
-  labels: {
-    currentPage: '*',
-    previousPage: '❮',
-    nextPage: '❯',
-    firstPage: 'first',
-    lastPage: 'last',
-    gapPage: '…'
-  },
-  features: {
-    hideAuto: false,
-    hideGaps: false,
-    hideAdjacent: false,
-    hideDisabled: true
-  }
-};
-
 document.addEventListener('DOMContentLoaded', function () {
-  var paginator1 = new Paginator(options);
-  paginator1.render('#pagination1');
+  var options = {
+    callback: yourCallback,
+    totalItems: 1000,
+    itemsPerPage: 10,
+    currentPage: 50,
+    options: {
+      outerPagesCount: 2,
+      innerPagesCount: 2
+    },
+    labels: {
+      currentPage: '*',
+      previousPage: '❮',
+      nextPage: '❯',
+      firstPage: 'first',
+      lastPage: 'last',
+      gapPage: '…'
+    },
+    features: {
+      hideAuto: false,
+      hideGaps: false,
+      hideAdjacent: false,
+      hideDisabled: true
+    }
+  };
+
+  var paginator = new Paginator(options);
+  paginator.render('#pagination');
+
+  function yourCallback() {
+    var args = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+    console.info(args);
+    paginator.currentPage = args.page;
+    paginator.update('#pagination');
+    return args;
+  }
 });
-
-//options.labels.previousPage = 'previous';
-//options.labels.nextPage = 'next';
-//options.options.outerPagesCount = 0;
-//options.options.innerPagesCount = 0;
-//var paginator2 = new Paginator(options);
-//paginator2.render(''#pagination2');
-
-//options.labels.previousPage = null;
-//options.labels.nextPage = null;
-//options.labels.firstPage = 'first';
-//options.labels.lastPage = 'last';
-//options.options.outerPagesCount = 2;
-//options.options.innerPagesCount = 2;
-//var paginator3 = new Paginator(options);
-//paginator3.render('#pagination3');
-
-//var options = {
-//  callback: yourCallback,
-//  totalItems: 100,
-//  itemsPerPage: 10,
-//  currentPage: 1,
-//  features: {
-//    autoHide: true,
-//    hideDisabled: true
-//  }
-//};
-//var paginator4 = new Paginator(options);
-//paginator4.render(''#pagination4');
-
-function yourCallback() {
-  var args = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-
-  console.info(args);
-  //paginator1.currentPage = args.page;
-  //paginator1.update('#pagination1');
-  //return args;
-}
